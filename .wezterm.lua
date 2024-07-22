5ee41e66f04f9d8837abcdd5632789305dc4041a
martindotexe <martin@solhaugfoto.no>
2024-07-22 22:17:22 +0200

    refactor: new theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,7 +15,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "3024 (base16)"
+config.color_scheme = "tokyonight_night"
 config.font_size = 18.0
 config.native_macos_fullscreen_mode = true
 config.font = wezterm.font("JetBrains Mono", { italic = false })
