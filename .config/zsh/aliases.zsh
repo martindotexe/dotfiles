51c68e2d3cf7598e436210c76ebd4e46390df82c
martindotexe <martinss2000@pm.me>
2025-08-03 01:39:26 +0200

    Some handy aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,2 +1,10 @@
 alias shutdown='sudo shutdown now'
 alias restart='sudo reboot'
+
+# ls
+alias ls='ls --color=auto'
+alias l='ls -l'
+alias ll='ls -lahF'
+alias lls='ls -lahFtr'
+alias la='ls -A'
+alias lc='ls -CF'
