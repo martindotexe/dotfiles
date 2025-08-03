4d565771c789d5df2afccf7e41cf6f40aff8d167
martindotexe <martinss2000@pm.me>
2025-08-03 12:59:00 +0200

    Transparent background in neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Colors
+vim.o.termguicolors = true
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
