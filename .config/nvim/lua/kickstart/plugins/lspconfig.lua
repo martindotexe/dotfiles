c0f439eb8ba82512a7099fd2f0148ebe297bb300
martindotexe <martinss2000@pm.me>
2025-08-04 16:53:14 +0200

    Added Svelte ++ to LSP and conform

--- .config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ .config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -219,30 +219,30 @@ return {
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				-- clangd = {},
+				svelte = {
+					filetypes = { "svelte" },
+					settings = {
+						svelte = {
+							plugin = {
+								svelte = {
+									defaultScriptLanguage = "typescript",
+								},
+							},
+						},
+					},
+				},
+				ts_ls = {},
+				tailwindcss = {},
+				emmet_ls = {
+					filetypes = { "html", "css", "scss", "svelte" },
+				},
 				gopls = {},
-				-- pyright = {},
-				-- rust_analyzer = {},
-				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--
-				-- Some languages (like typescript) have entire language plugins that can be useful:
-				--    https://github.com/pmizio/typescript-tools.nvim
-				--
-				-- But for many setups, the LSP (`ts_ls`) will work just fine
-				-- ts_ls = {},
-				--
-
 				lua_ls = {
-					-- cmd = { ... },
-					-- filetypes = { ... },
-					-- capabilities = {},
 					settings = {
 						Lua = {
 							completion = {
 								callSnippet = "Replace",
 							},
-							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-							-- diagnostics = { disable = { 'missing-fields' } },
 						},
 					},
 				},
